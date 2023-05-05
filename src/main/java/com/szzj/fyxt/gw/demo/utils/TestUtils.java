2fed715db59428c887899bf1e7409cde2d629310
丁小波 <814995329@qq.com>
2023-05-05 09:20:23 +0800

    test提交1

--- src/main/java/com/szzj/fyxt/gw/demo/utils/TestUtils.java
+++ src/main/java/com/szzj/fyxt/gw/demo/utils/TestUtils.java
@@ -12,7 +12,7 @@ public class TestUtils {
     public static String sendRequest(String url, String content) {
         HttpUtil httpUtil = HttpUtil.getInstance();
         content = AESSymmtricEncrypt.encrypt(content, StartConfig.SECRET);
-        //content = "62CV0gVfECb0g2jSLnEerOC7wDLPQJ5TkAK/bvP7naqjcVdbP3kSFRqjH8KR8XPCn/bEpeFa55w/4CjEtp3gTw==";
+        //content = "62CV0gVfECb0g2jSLnEerOC7wDLPQJ5TkAK/bvP7naqjcVdbP3kSFRqjH8KR8XPCn/bEpeFa55w/4CjEtp3gTw==21`21`21`12`";
         System.out.println(content);
         Map<String, String> header = new HashMap<>();
         header.put("APP-ID", StartConfig.APP_ID);
